8b70d6fc8473beff00bdf7deb4360843b41503c8
blyavova <grushvo3@gmail.com>
2023-09-24 20:58:33 +0300

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,9 @@
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+import {galleryItems} from './gallery-items.js'
 // Add imports above this line
-import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
@@ -11,9 +13,9 @@ const markup = galleryItems
   .map(
     ({ preview, original, description }) => `
 <li class="gallery__item">
-   <a class="gallery__link" href="${original}">
+  <a class="gallery__link" href="${original}">
       <img class="gallery__image" src="${preview}" alt="${description}" />
-   </a>
+  </a>
 </li>
 `
   )
